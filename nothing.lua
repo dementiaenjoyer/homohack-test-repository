3f2753020e96a061820f3aaf170becdd12ecdc52
dementiaenjoyer <81053531+dementiaenjoyer@users.noreply.github.com>
2026-06-05 19:02:34 +0200

    Update nothing.lua

--- nothing.lua
+++ nothing.lua
@@ -1,1 +1,9 @@
-print( DrawingImmediate );
+local Workspace = game : GetService( "Workspace" );
+local CurrentCamera = Workspace.CurrentCamera;
+
+local Center = CurrentCamera.ViewportSize * .5;
+local White = Color3.new( 1, 1, 1 );
+
+DrawingImmediate.GetPaint( 1 ) : Connect( function( )
+    DrawingImmediate.Circle( Center, 100, White, 1, 100, 1 );
+end )
